054fe49a6ebce29feae22613d8a6f733945156e3
Frank Martin Zelindro <frank.zelindro@gmail.com>
2022-09-21 12:40:56 +0200

    Create a script for environment setup
    This version of the scrip was tested on Ubuntu 22.04 and Mint 21

---
+++ prepare.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+if [[ $(id -u) > 0 ]]; then
+    echo "You must run this script as root."
+    exit 1
+fi
+
+# Install dependencies for Ubuntu 22.04 / Mint 21
+function install_pkg {
+    NEW_PKGS=()
+    INSTALLED_PKGS=()
+    for PKG in $@ ; do
+        if [[ 0 -eq $(dpkg-query -W -f='${Status}' $PKG 2>/dev/null | grep -c "ok installed") ]]; then
+            NEW_PKGS+=($PKG)
+        else
+            INSTALLED_PKGS+=($PKG)
+        fi
+    done
+    if [[ ${#NEW_PKGS[@]} -gt 0 ]]; then
+        apt install --assume-yes ${NEW_PKGS[@]}
+    fi
+    if [[ ${#INSTALLED_PKGS[@]} -gt 0 ]]; then
+        echo "<${INSTALLED_PKGS[@]}> already installed"
+    fi
+}
+
+install_pkg \
+    build-essential \
+    cmake \
+    libsdl2-dev
